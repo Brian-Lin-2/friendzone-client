0aa63715fa58406421a0b1c4e4a94fc4d9937f71
Brian <linbrian24@gmail.com>
2024-07-06 18:24:00 -0400

    Start basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Signup from "./components/home/Signup";
+import Login from "./components/home/Login";
+import User from "./components/user/User";
+
 export default function App() {
-  return <h1 className="">Hello</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="user" element={<User />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
